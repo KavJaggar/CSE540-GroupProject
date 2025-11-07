360ab113748455c4d252ea6ffe6105d5bd0c859c
Kavinkumaar Jaganathan <108050383+Kavinislyfeyall@users.noreply.github.com>
2025-11-06 17:15:52 -0700

    commented .sol draft code

--- group-proj-dapp/contracts/SupplyChain.sol
+++ group-proj-dapp/contracts/SupplyChain.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/// @title SupplyChainProvenance (Draft)
+/// @title SupplyChainProvenance (Draft/ Not Final)
 /// @notice Blockchain-based product tracking for the CSE540 project.
 
 contract SupplyChainProvenance {
-    enum Status { Unknown, Originated, Shipped, InStorage, Delivered }
 
+    //This enum lists all of the different possible status tags that a product can have at any point along the supply chain
+    enum Status { Unknown, Ordered, Shipped, InStorage, Delivered }
+
+    //Product struct storing all of the different notable attributes of a product while in the supply chain
     struct Product {
         uint256 id;
         address owner;
@@ -15,48 +18,60 @@ contract SupplyChainProvenance {
         Status status;
         uint256 createdAt;
     }
-
+    //Creates a counter variable to assign new product ids by just incrementing up by 1 after the assignment
     uint256 private nextId = 1;
+
+    //Creates mappings for all of the products so that they can be easily retrieved. Also creates mappings for holding the role assignments of
+    //manufacturers, distributors, and retailers(usually only 1)
     mapping(uint256 => Product) public products;
-    mapping(address => bool) public producers;
+    mapping(address => bool) public manufacturer;
     mapping(address => bool) public distributors;
     mapping(address => bool) public retailers;
+
+    //variable to store the address of the admin which is just set to msg.sender in the constructor later
     address public admin;
 
+    //sets up the three events that will be emitted. one for a product being created. one for the ownership of a product being transferred to someone else along
+    //the supply chain. and one for the status of the product being updated to one in the status enum declared above.
     event ProductCreated(uint256 indexed id, address indexed owner, string batchId);
     event OwnershipTransferred(uint256 indexed id, address indexed from, address indexed to);
     event StatusUpdated(uint256 indexed id, Status status);
 
+    //creates a modifier to use for functions only meant to be ran by the admin
     modifier onlyAdmin() {
         require(msg.sender == admin, "Only admin");
         _;
     }
 
-    modifier onlyProducer() {
-        require(producers[msg.sender], "Only producer");
+    //creates a modifier for functions only meant to be ran by manufacturers
+    modifier onlyManufacturer() {
+        require(manufacturer[msg.sender], "Only producer");
         _;
     }
 
+    //this constructor sets the deployer of the contract to the admin when it is first deployed. gives us permission as the admin when we deploy.
     constructor() {
         admin = msg.sender;
     }
 
-    function setProducer(address _addr, bool _ok) external onlyAdmin {
-        producers[_addr] = _ok;
+    //function allowing only the admin to assign people as manufacturers
+    function setManufacturer(address _addr, bool _ok) external onlyAdmin {
+        manufacturer[_addr] = _ok;
     }
 
+    //function allowing only the admin to assign people as distributors
     function setDistributor(address _addr, bool _ok) external onlyAdmin {
         distributors[_addr] = _ok;
     }
 
+    //function allowing only the admin to assign people as retailers
     function setRetailer(address _addr, bool _ok) external onlyAdmin {
         retailers[_addr] = _ok;
     }
 
-    function registerProduct(string calldata batchId, string calldata metadataUri)
-        external
-        onlyProducer
-        returns (uint256)
+    //function allowing only manufacturers to register products. the function registers the product based on the provided parameters
+    //and then returns the id of the product that was just registered
+    function registerProduct(string calldata batchId, string calldata metadataUri) external onlyManufacturer returns (uint256)
     {
         uint256 id = nextId++;
         products[id] = Product({
@@ -64,13 +79,15 @@ contract SupplyChainProvenance {
             owner: msg.sender,
             batchId: batchId,
             metadataUri: metadataUri,
-            status: Status.Originated,
+            status: Status.Ordered,
             createdAt: block.timestamp
         });
         emit ProductCreated(id, msg.sender, batchId);
         return id;
     }
 
+    //function that allows the owner of a product to transfer its ownership to someone else. emits an event stating the id of the product and who transferred its ownership
+    //and to who it was transferred to
     function transferOwnership(uint256 id, address to) external {
         Product storage p = products[id];
         require(p.id != 0, "Product does not exist");
@@ -79,17 +96,20 @@ contract SupplyChainProvenance {
         emit OwnershipTransferred(id, msg.sender, to);
     }
 
+    //function that allows the owner of the product to update the status of the product. also allows the admin to do so in the event of the product being misplaced or
+    //lost somewhere along the way and requiring manual intervention to correct the information. emits an event stating which product was updated and what the status was updated to.
     function updateStatus(uint256 id, Status newStatus) external {
         Product storage p = products[id];
         require(p.id != 0, "Product does not exist");
         require(
-            msg.sender == p.owner || producers[msg.sender] || distributors[msg.sender] || retailers[msg.sender],
+            msg.sender == p.owner || msg.sender == admin,
             "Not authorized"
         );
         p.status = newStatus;
         emit StatusUpdated(id, newStatus);
     }
 
+    //returns a product given a product id. can be called by anyone with access.
     function getProduct(uint256 id) external view returns (Product memory) {
         return products[id];
     }
